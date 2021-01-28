35c113976410a1d678f502e576ae8c8cb510f9e3
Toby Dylan Hocking <tdhock5@gmail.com>
2021-01-28 06:13:56 -0700

    simplify

--- interpreter.py
+++ interpreter.py
@@ -1,14 +1,14 @@
-import code, sys, fileinput
-sys.ps1 = "\n>>>"
-fi = fileinput.input(sys.argv[1])
+import code
 class FileConsole(code.InteractiveConsole):
     """Emulate python console but use file instead of stdin"""
     def raw_input(self, prompt):
-        l = fi.readline()
-        if l=="":
+        line = f.readline()
+        if line=="":
             raise EOFError()
-        print(prompt, l.replace("\n", ""))
-        return l
-fc = FileConsole()
-fc.interact(banner="", exitmsg="")
+        print(prompt, line.replace("\n", ""))
+        return line
+import sys
+sys.ps1 = "\n>>>"
+f = open(sys.argv[1])
+FileConsole().interact(banner="", exitmsg="")
 
