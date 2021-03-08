7c86b1fc9a72f191ea577e82a2181d165db0e21e
Toby Dylan Hocking <tdhock5@gmail.com>
2021-03-08 09:02:54 -0700

    proj 3

--- programs/torch-part1/single_hidden_layer.py
+++ programs/torch-part1/single_hidden_layer.py
@@ -6,7 +6,7 @@ from torch.utils.tensorboard import SummaryWriter
 from urllib.request import urlretrieve
 
 writer_dict = {}
-for s in "train", 'valid':
+for s in "subtrain", 'validation':
     writer_dict[s] = SummaryWriter("runs/"+s)
 
 def to_torch(s):
@@ -14,14 +14,12 @@ def to_torch(s):
     return torch.from_numpy(a.reshape(a.shape[0],1).astype(numpy.float32))    
 
 class Net(torch.nn.Module):
-
     def __init__(self):
         super(Net, self).__init__()
         n_hidden = 20
         self.act = torch.nn.Sigmoid()
         self.hidden = torch.nn.Linear(1, n_hidden)
         self.out = torch.nn.Linear(n_hidden, 1)
-
     def forward(self, x):
         x = self.act(self.hidden(x))
         x = self.out(x)
@@ -31,21 +29,24 @@ criterion = torch.nn.MSELoss()
 n_folds = 5
 data_dict = {}
 for pattern in "sin", "linear", "quadratic":
+    # first download data set
     f = "data_%s.csv" % pattern
     if not os.path.exists(f):
         u = "https://raw.githubusercontent.com/tdhock/2020-yiqi-summer-school/master/" + f
         urlretrieve(u, f)
     df = pandas.read_csv(f)
     nrow = df.shape[0]
+    # randomly assign fold IDs
     numpy.random.seed(123)
     fold_vec = numpy.repeat(
         numpy.arange(n_folds),
         nrow/n_folds)
     numpy.random.shuffle(fold_vec)
-    valid_fold = 0
-    by_set = df.set_index(numpy.where(fold_vec == valid_fold, "valid", "train"))
+    # use fold=0 as validation set
+    validation_fold = 0
+    by_set = df.set_index(numpy.where(fold_vec == validation_fold, "validation", "subtrain"))
     tensors = {}
-    for s in "valid", "train":
+    for s in "validation", "subtrain":
         set_data = {}
         for xy in "x", "y":
             set_data[xy] = to_torch(by_set.loc[s, xy])
@@ -58,13 +59,13 @@ for pattern in "sin", "linear", "quadratic":
     max_epochs = 100
     for epoch in range(max_epochs):
         loss_dict = {"epoch":epoch, "pattern":pattern}
-        for s in "train", "valid":
+        for s in "subtrain", "validation":
             pred = net(tensors[s]["x"])
             loss = criterion(pred, tensors[s]["y"])
             loss_dict[s] = loss
             writer_dict[s].add_scalar(pattern+'/Loss', loss, epoch)
             # TODO try add_image https://pytorch.org/tutorials/intermediate/tensorboard_tutorial.html#tracking-model-training-with-tensorboard
-            if s is "train":
+            if s is "subtrain":
                 def closure():
                     optimizer.zero_grad()
                     pred = net(tensors[s]["x"])
@@ -72,4 +73,4 @@ for pattern in "sin", "linear", "quadratic":
                     loss.backward()
                     return loss                
                 optimizer.step(closure)    # Does the update
-        print("pattern=%(pattern)s epoch=%(epoch)d train=%(train)f valid=%(valid)f" % loss_dict)
+        print("pattern=%(pattern)s epoch=%(epoch)d subtrain=%(subtrain)f validation=%(validation)f" % loss_dict)
